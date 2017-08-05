a865661e9c690f5a648a07b90ccd94a2ba63a697
Harrison McCord <harrison.mccord@gmail.com>
2017-08-05 09:58:14 -0400

    cleaning

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
 // Tells node that we are creating an "express" server
 var app = express();
+
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
+
 // BodyParser makes it possible for our server to interpret data sent to it.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
